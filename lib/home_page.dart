136c715732965c2df06435f2f714a4ae20023ee2
Christopher Paulino <164887@p.uapa.edu.do>
2019-11-10 11:58:01 -0400

    nav-agregated

---
+++ lib/home_page.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+
+
+  @override
+  State createState() =>HomeState();
+}
+
+class HomeState extends State<HomePage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Pasar Lista')
+          ), BottomNavigationBarItem(
+              icon: Icon(Icons.import_contacts),
+              title: Text('Cursos')
+          ), BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Administrar')
+          )
+        ],
+      ),
+      body: Center(
+        child: Text("klk"),
+      ),
+    );
+  }
+}
